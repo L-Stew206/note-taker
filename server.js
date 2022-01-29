312e735b54c1a74a5455dfb944dce1dc9bba2454
Luke <lukeconrad.stew@gmail.com>
2022-01-28 22:47:41 -0800

    varibles

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const { v4: uuidv4 } = require('uuid');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+const notes = require ('.db/db.json');
 
 // send the files 'notes.html'
 app.get('/notes', (req,res) => {
 
-    res.sendFile();
+    res.sendFile( path.join(__dirname, 'public/notes.html'));
 
 });
 
@@ -22,10 +28,14 @@ res.json();
  
 app.post('/api/notes', (req,res) => {
 
+    // access the note data that was sent
+    const newNote = req.body;
     // create data
     // access the new note data from'req'
     //push it to my existsing list of notes
     // write my updated notes list to the 'db.json' file
+
+    res.json('mesage for me')
 
 });
 
